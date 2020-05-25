2f5acd2d40677136957100e591b0b949fefc7c06
DEADB17 <DEADB17@gmail.com>
2020-05-25 14:50:58 -0400

    Allow multiple rules

--- test/index.js
+++ test/index.js
@@ -55,32 +55,42 @@ function kz(strings, ...keys) {
     ? keys.map((it, i) => strings[i] + it.toString())
     : strings
   ).join("");
-  const args = str.split(":").map((it) => it.trim());
-  const n = args.length;
-  let at, pseudo, prop, val;
-  if (n < 2) throw new Error("klaz: prop and val must be defined");
-  else if (2 === n) {
-    [prop, val] = args;
-    at = pseudo = NONE;
-  } else if (4 <= n) {
-    [at, pseudo, prop, val] = args;
-  } else {
-    // 3 elements
-    val = args.pop();
-    prop = args.pop();
-    if (args[0] in breakpoints) {
-      at = args[0];
-      pseudo = NONE;
+  /** @type {string[]}*/
+  const acc = [];
+  const res = str.split(/\s*;\s*/).reduce((acc, it) => {
+    const rule = it.trim();
+    if (rule.length < 1) return acc;
+
+    const args = rule.split(/\s*:\s*/);
+    const n = args.length;
+    let at, pseudo, prop, val;
+    if (n < 2) throw new Error("klaz: prop and val must be defined");
+    else if (2 === n) {
+      [prop, val] = args;
+      at = pseudo = NONE;
+    } else if (4 <= n) {
+      [at, pseudo, prop, val] = args;
     } else {
-      at = NONE;
-      pseudo = args[0];
+      // 3 elements
+      val = args.pop();
+      prop = args.pop();
+      if (args[0] in breakpoints) {
+        at = args[0];
+        pseudo = NONE;
+      } else {
+        at = NONE;
+        pseudo = args[0];
+      }
     }
-  }
-  if (val == null) throw new Error("klaz: val must be defined");
-  if (!(prop && prop in props)) throw new Error("klaz: invalid prop");
-  if (!(at in breakpoints)) throw new Error("klaz: Invalid break-point");
-  if (!(pseudo in pseudos)) throw new Error("klaz: Invalid pseudo");
-  return classNameOf(at, pseudo, prop, val);
+    if (val == null) throw new Error("klaz: val must be defined");
+    if (!(prop && prop in props)) throw new Error("klaz: invalid prop");
+    if (!(at in breakpoints)) throw new Error("klaz: Invalid break-point");
+    if (!(pseudo in pseudos)) throw new Error("klaz: Invalid pseudo");
+
+    acc.push(classNameOf(at, pseudo, prop, val));
+    return acc;
+  }, acc);
+  return res.join(" ");
 }
 
 // /////////////////////////////////////////////////////////////////////////////
@@ -107,17 +117,41 @@ console.log("ok");
 
 ////////////////////////////////////////////////////////////////////////////////
 
-console.log("kz");
+console.log("kz: Single rule");
 
 assert.equal(kz`color:red`, "X01yred");
-assert.throws(() => kz`one`); // too few
-assert.throws(() => kz``); // too few
+assert.throws(() => kz`one`, "Too few: Single arg");
+assert.equal(kz``, "", "OK: no arguments");
 assert.equal(kz`margin-bottom:-6rem`, "X04i-6rem");
 assert.equal(kz`sm:margin-bottom:-6rem`, "s04i-6rem");
 assert.equal(kz`hover:margin-bottom:-6rem`, "Xd4i-6rem");
 assert.equal(kz`sm:hover:margin-bottom:-6rem`, "sd4i-6rem");
 assert.equal(kz`md:background-size:cover`, "m00n25");
 assert.equal(kz`lg : background-size : auto 6px`, "l00n0j6px");
-assert.equal(kz`background-size : auto, 50%, contain`, "X00n0j\,50\%\,1y");
+assert.equal(kz`background-size : auto, 50%, contain`, "X00n0j,50%,1y");
 
+console.log("ok");
+
+////////////////////////////////////////////////////////////////////////////////
+
+console.log("kz: Multiple rules");
+
+{
+  const actual = kz`
+color: red;
+padding-top: 0;
+text-decoration: none
+`;
+  const expected = "X01yred X05q0 X07h7e";
+  assert.equal(actual, expected);
+}
+{
+  const actual = kz`;
+color: red;;
+padding-top: 0;
+text-decoration: none;
+`;
+  const expected = "X01yred X05q0 X07h7e";
+  assert.equal(actual, expected, "extra ;");
+}
 console.log("ok");

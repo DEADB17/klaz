34e4593d1da53d12082c21b0fbc6ceb827d89359
DEADB17 <DEADB17@gmail.com>
2020-05-25 19:36:06 -0400

    Reorganize files

--- test/index.js
+++ test/index.js
@@ -1,9 +1,8 @@
+import { addRule, escapeValue, formatValue, createKlaz } from "../lib/klaz.js";
+import { NONE } from "../lib/tabs.js";
 import { strict as assert } from "assert";
-import { NONE, pseudos, props, values } from "../lib/tabs.js";
 
-/** @typedef {Record<string,string>} Brk */
-
-/** @type {Brk} */
+/** @type {import('../lib/klaz.js').Brk} */
 const breakpoints = {
   [NONE]: "X",
   sm: "s",
@@ -11,108 +10,68 @@ const breakpoints = {
   lg: "l",
 };
 
-// TODO(db17): Is this spec compliant?
-/** @arg {string} val */
-function escapeValue(val) {
-  return val.replace(/[^_a-zA-Z0-9-]/g, "$&");
+// /////////////////////////////////////////////////////////////////////////////
+
+console.log("addRule");
+
+{
+  let actual, expected;
+
+  actual = addRule(null, NONE, NONE, "margin", "0");
+  expected = {
+    [NONE]: {
+      margin: {
+        [NONE]: {
+          "0": "0",
+        },
+      },
+    },
+  };
+  assert.deepEqual(actual, expected, "creates missing");
+
+  actual = addRule(actual, NONE, NONE, "margin", "0");
+  expected = {
+    [NONE]: {
+      margin: {
+        [NONE]: {
+          "0": "0",
+        },
+      },
+    },
+  };
+  assert.deepEqual(actual, expected, "is idempotent");
+
+  actual = addRule(actual, NONE, NONE, "margin", "2px");
+  actual = addRule(actual, NONE, "first", "display", "flex");
+  actual = addRule(actual, "md", "hover", "overflow", "auto");
+  expected = {
+    [NONE]: {
+      margin: {
+        [NONE]: {
+          "2px": "2px",
+          "0": "0",
+        },
+      },
+      display: {
+        first: {
+          flex: "flex",
+        },
+      },
+    },
+    md: {
+      overflow: {
+        hover: {
+          auto: "auto",
+        },
+      },
+    },
+  };
+  assert.deepEqual(actual, expected, "add variations");
 }
 
-/** @arg {string} val */
-function formatValue(val) {
-  /** @type {string[]}*/
-  const acc = [];
-  return val
-    .replace(/,/g, " , ") // Ensure that , are split but preserved
-    .split(/\s+/)
-    .reduce((acc, it) => {
-      const str = it.trim();
-      if (str.length < 1) return acc;
-      acc.push(values[it] || escapeValue(it));
-      return acc;
-    }, acc)
-    .join("");
-}
-
-// TODO(db17): break-points and pseudos are not really needed (Remove them)
-/**
- * @arg {Brk} brk
- * @arg {string} at
- * @arg {string} pseudo
- * @arg {string} prop
- * @arg {string} val
- */
-function createClassName(brk, at, pseudo, prop, val) {
-  const a = brk[at];
-  const s = pseudos[pseudo];
-  const p = props[prop];
-  const v = formatValue(val);
-  return a + s + p + v;
-}
-
-/**
- * @arg {Brk} brk
- * @arg {TemplateStringsArray} strings
- * @arg {(string|number)[]} keys
- */
-function klaz(brk, strings, ...keys) {
-  const str = (keys.length
-    ? keys.map((it, i) => strings[i] + it.toString())
-    : strings
-  ).join("");
-  /** @type {string[]}*/
-  const acc = [];
-  const res = str.split(/\s*;\s*/).reduce((acc, it) => {
-    const rule = it.trim();
-    if (rule.length < 1) return acc;
-
-    const args = rule.split(/\s*:\s*/);
-    const n = args.length;
-    let at, pseudo, prop, val;
-    if (n < 2) throw new Error("klaz: prop and val must be defined");
-    else if (2 === n) {
-      [prop, val] = args;
-      at = pseudo = NONE;
-    } else if (4 <= n) {
-      [at, pseudo, prop, val] = args;
-    } else {
-      // 3 elements
-      val = args.pop();
-      prop = args.pop();
-      if (args[0] in breakpoints) {
-        at = args[0];
-        pseudo = NONE;
-      } else {
-        at = NONE;
-        pseudo = args[0];
-      }
-    }
-    if (val == null) throw new Error("klaz: val must be defined");
-    if (!(prop && prop in props)) throw new Error("klaz: invalid prop");
-    if (!(at in breakpoints)) throw new Error("klaz: Invalid break-point");
-    if (!(pseudo in pseudos)) throw new Error("klaz: Invalid pseudo");
-
-    acc.push(createClassName(brk, at, pseudo, prop, val));
-    return acc;
-  }, acc);
-  return res.join(" ");
-}
-
-/**
- * @arg {Brk} breakpoints
- */
-function createKlaz(breakpoints) {
-  /**
-   * @arg {TemplateStringsArray} strings
-   * @arg {(string|number)[]} keys
-   */
-  function kz(strings, ...keys) {
-    return klaz(breakpoints, strings, ...keys);
-  }
-  return kz;
-}
+console.log("ok");
 
 // /////////////////////////////////////////////////////////////////////////////
-// Tests
 
 console.log("escapeValue");
 

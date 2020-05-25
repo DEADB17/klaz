75e0f8eff6874c7f1e44b537cc83d0f28a941fac
DEADB17 <DEADB17@gmail.com>
2020-05-25 13:46:34 -0400

    Make classname generation compact

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 import { strict as assert } from "assert";
-import { NONE, pseudos, props } from "../lib/tabs.js";
+import { NONE, pseudos, props, values } from "../lib/tabs.js";
 
 /** @type {Record<string,string>} */
 const breakpoints = {
@@ -9,6 +9,29 @@ const breakpoints = {
   lg: "l",
 };
 
+// TODO(db17): Is this spec compliant?
+/** @arg {string} val */
+function escapeValue(val) {
+  return val.replace(/[^_a-zA-Z0-9-]/g, "$&");
+}
+
+/** @arg {string} val */
+function formatValue(val) {
+  /** @type {string[]}*/
+  const acc = [];
+  return val
+    .replace(/,/g, " , ") // Ensure that , are split but preserved
+    .split(/\s+/)
+    .reduce((acc, it) => {
+      const str = it.trim();
+      if (str.length < 1) return acc;
+      acc.push(values[it] || escapeValue(it));
+      return acc;
+    }, acc)
+    .join("");
+}
+
+// TODO(db17): break-points and pseudos are not really needed (Remove them)
 /**
  * @arg {string} at
  * @arg {string} pseudo
@@ -19,7 +42,7 @@ function classNameOf(at, pseudo, prop, val) {
   const a = breakpoints[at];
   const s = pseudos[pseudo];
   const p = props[prop];
-  const v = val; // TODO(db17): known value or escape
+  const v = formatValue(val);
   return a + s + p + v;
 }
 
@@ -63,6 +86,27 @@ function kz(strings, ...keys) {
 // /////////////////////////////////////////////////////////////////////////////
 // Tests
 
+console.log("escapeValue");
+
+assert.equal(escapeValue("-ab-cd_0"), "-ab-cd_0");
+assert.equal(escapeValue("123456"), "123456");
+assert.equal(escapeValue("~!@#$%^&*()+="), "~!@#$%^&*()+=");
+
+console.log("ok");
+
+////////////////////////////////////////////////////////////////////////////////
+
+console.log("formatValue");
+
+assert.equal(formatValue("1px"), "1px");
+assert.equal(formatValue("auto"), "0j");
+assert.equal(formatValue("auto auto auto"), "0j0j0j");
+assert.equal(formatValue("auto,auto,auto"), "0j,0j,0j");
+
+console.log("ok");
+
+////////////////////////////////////////////////////////////////////////////////
+
 console.log("kz");
 
 assert.equal(kz`color:red`, "X01yred");
@@ -72,11 +116,8 @@ assert.equal(kz`margin-bottom:-6rem`, "X04i-6rem");
 assert.equal(kz`sm:margin-bottom:-6rem`, "s04i-6rem");
 assert.equal(kz`hover:margin-bottom:-6rem`, "Xd4i-6rem");
 assert.equal(kz`sm:hover:margin-bottom:-6rem`, "sd4i-6rem");
-assert.equal(kz`md:background-size:cover`, "m00ncover");
-assert.equal(kz`lg : background-size : auto 6px`, "l00nauto 6px");
-assert.equal(
-  kz`background-size : auto, 50%, contain`,
-  "X00nauto, 50%, contain"
-);
+assert.equal(kz`md:background-size:cover`, "m00n25");
+assert.equal(kz`lg : background-size : auto 6px`, "l00n0j6px");
+assert.equal(kz`background-size : auto, 50%, contain`, "X00n0j\,50\%\,1y");
 
 console.log("ok");

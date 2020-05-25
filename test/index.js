ec616889f48a70bf36a60edaf56e9790ecf64fd2
DEADB17 <DEADB17@gmail.com>
2020-05-25 11:04:36 -0400

    Update classname generation

--- test/index.js
+++ test/index.js
@@ -1,24 +1,26 @@
 import { strict as assert } from "assert";
-import { pseudos } from "../lib/tabs.js";
+import { NONE, pseudos, props } from "../lib/tabs.js";
 
+/** @type {Record<string,string>} */
 const breakpoints = {
-  sm: true,
-  md: true,
-  lg: true,
+  [NONE]: "X",
+  sm: "s",
+  md: "m",
+  lg: "l",
 };
 
-const NONE = "X";
-
-const arrayJoin = Array.prototype.join;
-
 /**
- * @arg {string} _at
- * @arg {string} _pseudo
- * @arg {string} _prop
- * @arg {string} _val
+ * @arg {string} at
+ * @arg {string} pseudo
+ * @arg {string} prop
+ * @arg {string} val
  */
-function classNameOf(_at, _pseudo, _prop, _val) {
-  return arrayJoin.call(arguments, "_");
+function classNameOf(at, pseudo, prop, val) {
+  const a = breakpoints[at];
+  const s = pseudos[pseudo];
+  const p = props[prop];
+  const v = val; // TODO(db17): known value or escape
+  return a + s + p + v;
 }
 
 /**
@@ -39,9 +41,6 @@ function kz(strings, ...keys) {
     at = pseudo = NONE;
   } else if (4 <= n) {
     [at, pseudo, prop, val] = args;
-    if (!isValid(at, breakpoints)) throw new Error("klaz: Invalid break-point");
-    if (!isValid(pseudo, pseudos))
-      throw new Error("klaz: Invalid pseudo-class/element");
   } else {
     // 3 elements
     val = args.pop();
@@ -49,43 +48,35 @@ function kz(strings, ...keys) {
     if (args[0] in breakpoints) {
       at = args[0];
       pseudo = NONE;
-    } else if (args[0] in pseudos) {
+    } else {
       at = NONE;
       pseudo = args[0];
-    } else throw new Error("klaz: Invalid break-point or pseudo-class/element");
+    }
   }
-  if (prop == null || val == null) {
-    throw new Error("klaz: prop and val must be defined");
-  }
+  if (val == null) throw new Error("klaz: val must be defined");
+  if (!(prop && prop in props)) throw new Error("klaz: invalid prop");
+  if (!(at in breakpoints)) throw new Error("klaz: Invalid break-point");
+  if (!(pseudo in pseudos)) throw new Error("klaz: Invalid pseudo");
   return classNameOf(at, pseudo, prop, val);
-}
-
-/**
- * @arg {any} key
- * @arg {Record<string, any>} set
- */
-function isValid(key, set) {
-  return typeof key === "string" && 0 < key.length && key in set;
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // Tests
 
-assert.equal(kz`color:red`, "X_X_color_red");
+console.log("kz");
+
+assert.equal(kz`color:red`, "X01yred");
 assert.throws(() => kz`one`); // too few
 assert.throws(() => kz``); // too few
-assert.equal(kz`margin-bottom:-6rem`, "X_X_margin-bottom_-6rem");
-assert.equal(kz`sm:margin-bottom:-6rem`, "sm_X_margin-bottom_-6rem");
-assert.equal(kz`hover:margin-bottom:-6rem`, "X_hover_margin-bottom_-6rem");
-assert.equal(kz`sm:hover:margin-bottom:-6rem`, "sm_hover_margin-bottom_-6rem");
-assert.equal(kz`md:background-size:cover`, "md_X_background-size_cover");
-assert.equal(
-  kz`lg : background-size : auto 6px`,
-  "lg_X_background-size_auto 6px"
-);
+assert.equal(kz`margin-bottom:-6rem`, "X04i-6rem");
+assert.equal(kz`sm:margin-bottom:-6rem`, "s04i-6rem");
+assert.equal(kz`hover:margin-bottom:-6rem`, "Xd4i-6rem");
+assert.equal(kz`sm:hover:margin-bottom:-6rem`, "sd4i-6rem");
+assert.equal(kz`md:background-size:cover`, "m00ncover");
+assert.equal(kz`lg : background-size : auto 6px`, "l00nauto 6px");
 assert.equal(
   kz`background-size : auto, 50%, contain`,
-  "X_X_background-size_auto, 50%, contain"
+  "X00nauto, 50%, contain"
 );
 
 console.log("ok");
